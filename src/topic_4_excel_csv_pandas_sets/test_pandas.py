8151ba5c9bcd0eb3a432388e5658001328b15dbd
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-11-30 22:22:55 +0200

    student grades first version

---
+++ src/topic_4_excel_csv_pandas_sets/test_pandas.py
@@ -0,0 +1,5 @@
+# let's load pandas library and show its version
+import pandas as pd # standard alias for pandas is pd
+print(f"Pandas version: {pd.__version__}")
+# the script is over here
+# if we wanted something interactive we would want to use Jupyter Notebooks instead
