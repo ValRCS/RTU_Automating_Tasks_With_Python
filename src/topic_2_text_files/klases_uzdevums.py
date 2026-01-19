0a648b7b031aa4039f9c0124c4597150204886f7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-01-19 21:42:22 +0200

    Add functionality to clean and save poetry lines from veidenbaums.txt to new files, optimizing the process for large files.

--- src/topic_2_text_files/klases_uzdevums.py
+++ src/topic_2_text_files/klases_uzdevums.py
@@ -52,4 +52,54 @@ else:
 # iet cauri nolasītajam saturam
 # ja rinda ir dzeja (nav tukša un nesatur ***) tad pievienot to jaunajam sarakstam
 
+# 1. solis viens nolasam veidenbaums.txt
 
+# noklusētais režīms ir r - tātad lasīšanas
+with open("veidenbaums.txt", encoding="utf-8") as file:
+    all_rows = file.readlines() # ielasa visu atmiņā kā rindiņu sarakstu - ja ļoti liels fails tad neder
+    # fails vēl te vaļā
+# te jau fails ciet
+
+# jauns tukšs saraksts
+clean_rows = []
+
+# izejam cauri ar for ciklu visām oriģinām rindām un atsijājam vajadzīgās
+
+for row in all_rows: # iesim cauri visām rindiņām pa vienai
+    # šeit mums būs viena rindiņa, kāda nezinam, bet visas apstrādāsim secīgi
+    if len(row.strip()) > 0 and "***" not in row:
+        clean_rows.append(row) # append modificē clean_rows un pieliek row klāt kā jaunu elementu
+
+# cik oriģināla rindu
+print(f"Man ir {len(all_rows)} rindas pirmavota")
+# uzrakstīsim cik tad man ir tās tīrās dzejas rindas
+print(f"Man ir {len(clean_rows)} rindas dzeju")
+
+# atliek vien saglābat visu tīro jauna failā
+
+# sauksim to par tīro dzeju
+with open("veidenbaums_clean.txt", mode="w", encoding="utf-8") as file: # file is a new stream out
+    file.writelines(clean_rows)
+    # fails vēl te vaļā
+# te jau fails ciet
+print(f"Tīrā dzeja saglabāta failā veidenbaums_clean.txt.")
+
+
+# for large text files we can optimize this to open two files at once
+# vispirms atveram failu lasīšanai
+with open("veidenbaums.txt", encoding="utf-8") as infile:
+    # tad atveram jaunu failu rakstīšanai
+    with open("veidenbaums_clean_optimized.txt", mode="w", encoding="utf-8") as outfile:
+        # te ir abi faili atvērti
+        # ejam cauri katrai rindiņai no ieejas faila
+        for line in infile: # mēs tātad "dzenam" uz priekšu mūsu izejas failu pa vienai rindiņai
+            # pārbaudām vai rinda ir dzeja - te var mainīt loģiku kā gribat
+            if len(line.strip()) > 0 and "***" not in line:
+                # ja ir dzeja, ierakstām to izejas failā
+                outfile.write(line)
+        # izejas fails vēl te vaļā
+    # ieejas fails vēl te vaļā
+
+# te visi faili ir jau ciet
+
+print("Tīrā dzeja saglabāta failā veidenbaums_clean_optimized.txt.")
