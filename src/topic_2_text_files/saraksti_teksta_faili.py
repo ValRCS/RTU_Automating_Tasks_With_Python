1cd505c8b2294d36689028ed45abe983f73fd02c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-01-20 18:17:26 +0200

    Add initial script to outline today's plan and tasks related to list data structures and text file processing in Python

---
+++ src/topic_2_text_files/saraksti_teksta_faili.py
@@ -0,0 +1,7 @@
+# Plāns šodiena
+# Apskatīt saraksta datu struktūru Python valodā
+# apskatīt teksta failus un to apstrādi Python valodā
+
+print("Plāns šodienai:")
+print("- Apskatīt saraksta datu struktūru Python valodā")
+print("- Apskatīt teksta failus un to apstrādi Python valodā")
