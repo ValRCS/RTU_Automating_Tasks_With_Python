cb051ae8637447ad8218432a8e0391a4952facf0
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-01-20 18:38:46 +0200

    Enhance saraksti_teksta_faili.py with detailed explanations and examples of list usage in Python

--- src/topic_2_text_files/saraksti_teksta_faili.py
+++ src/topic_2_text_files/saraksti_teksta_faili.py
@@ -24,3 +24,29 @@ print(a4)
 # vai 10 000 vārdi?
 # vai pat 1 000 000 vārdi?
 # vai vēl vairāk?
+
+# risinājums ir saraksti (lists)
+# saraksti ir datu struktūra, kas ļauj glabāt vairākas vērtības vienā mainīgajā
+# saraksti ir ļoti ērti, jo mēs varam piekļūt katrai vērtībai pēc tās indeksa
+# varam sarakstus mainīt, pievienot jaunus elementus, dzēst elementus utt.
+# saraksti ir ļoti elastīgi un ērti lietojami
+
+# sāksim ar tukšu sarakstu
+saraksts = [] # tātad kvadrātiekavās norādām, ka tas ir saraksts
+
+# cik mums tur ir elementu?
+print(f"Sarakstā ir {len(saraksts)} elementi.")
+
+# pievienosim vienu elementu
+saraksts.append("Oskars") # append modificē sarakstu, pievienojot tam jaunu elementu beigās
+print(f"Sarakstā ir {len(saraksts)} elementi.")
+# pieliksim arī Valdis
+saraksts.append("Valdis")
+print(f"Sarakstā ir {len(saraksts)} elementi.")
+# un tagad apskatīsim visu sarakstu
+print(saraksts)
+
+# man varētu būt skaitļu saraksts to ir viegli uztaisit ar list un range
+# piemēram no 0 līdz 19
+skaitli = list(range(20)) # tātad pusgatavos skaitļus no range mēs pārvēršam par sarakstu ar list
+print(skaitli)
