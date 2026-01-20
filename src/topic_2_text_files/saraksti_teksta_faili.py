12b00ce8e5b9c8acf31dbd4d1a53f7c934b4d764
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-01-20 18:59:43 +0200

    Enhance saraksti_teksta_faili.py with additional examples of list indexing, slicing, and error handling in Python

--- src/topic_2_text_files/saraksti_teksta_faili.py
+++ src/topic_2_text_files/saraksti_teksta_faili.py
@@ -50,3 +50,74 @@ print(saraksts)
 # piemēram no 0 līdz 19
 skaitli = list(range(20)) # tātad pusgatavos skaitļus no range mēs pārvēršam par sarakstu ar list
 print(skaitli)
+
+# piekļuve individuāliem elementiem sarakstā
+# tam izmanot indeksu līdzigi string indeksime, abos pirmais elements ir ar indeksu 0
+print(saraksts[0]) # pirmais elements
+print(saraksts[1]) # otrais elements
+
+#kā būta ja mēģinātu piekļūt 3 elementam ar indeksu 2?
+# print(saraksts[2]) # šeit būskļūda jo sarakstā ir tikai 2 elementi ar indeksiem 0 un 1
+
+# ja neesam pārliecināti cik elementu ir sarakstā, varam izmantot len funkciju
+elementu_skaits = len(saraksts) # to jau zinam un tad if nosacījumā pārbaudīt
+if elementu_skaits > 2: # mēs prasam atļauju vispirms
+    print(saraksts[2]) # šeit vairs nebūs kļūda jo zinām ka ir vairāk par 2 elementiem
+else:
+    print("Sarakstā nav tik daudz elementu.")
+
+# otrs veids būtu ar try except
+try: # šeit mēs tā teikt "šaujam" vispirms un prasam piedošanu ja kļūda
+    print(saraksts[2]) # mēģinam piekļūt 3 elementam
+except IndexError:
+    print("Noķērām kļūdu. Sarakstā nav tik daudz elementu.") # ja ir kļūda, izpildās šis bloks
+
+# pieliksim Līgu sarakstam
+saraksts.append("Līga") # pievienojam vēl vienu elementu
+print(saraksts)
+# tagad nebūs vairs kļūdas piekļūstot 3 elementam
+print(saraksts[2]) # šeit viss ir kārtībā jo sarakstā ir tagad 3 elementi
+
+# tagad piekļusim 3 jam skaitlim
+print(skaitli[2]) # šeit viss ir kārtībā jo skaitļu sarakstā ir daudz elementu
+
+# pēdējais elements
+# mēs varētu darīt šādi rēķinat garumu un atņemt 1
+print(saraksts[len(saraksts)-1]) # sarežģīti bet strādā, ļoti nePythonic
+# daudz labāk izmantot negatīvo indeksu
+print(saraksts[-1]) # pēdējais elements
+print(skaitli[-1]) # pēdējais skaitlis
+
+# kā būtu ar priekšpēdējo elementu?
+print(saraksts[-2]) # priekšpēdējais elements
+print(skaitli[-2]) # priekšpēdējais skaitlis
+
+# vairāk par sarakstime no Google ko tas dod saviem darbiniekiem: https://developers.google.com/edu/python/lists
+
+# tagad apskatīsim vairākku elementu vienlaicīgu piekļuvi
+# tam izmantojam "slicing" - sagriešanu 
+
+# piemēram pirmos 4 skaitļus no skaitļu saraksta
+print(skaitli[0:4]) # no indeksa 0 līdz 4 (4 nav iekļauts)
+# vēl kompaktāk ir bez nulles jo tā ir noklusēta
+print(skaitli[:4]) # no sākuma līdz 4 (4 nav iekļauts)
+
+# kā būtu ar pēdejiem 4 skaitļiem?
+print(skaitli[-4:]) # no -4 līdz beigām
+
+# es varu salipināt kopā jaunā sarakstā piemēram pirmos 5 un pēdejos 7 skaitļus
+jauni_skaitli = skaitli[:5] + skaitli[-7:] # šeit + tiek izmantots divu sarakstu lipināšani iegūstot jaunu sarakstu
+print(jauni_skaitli)
+
+# kā būtu ar katru otru skaitli
+print(skaitli[::2]) # no sākuma līdz beigām ar soli 2
+# kā būtu ar 2 elementu un atkal pa 2
+print(skaitli[1::3]) # no indeksa 1 līdz beigām ar soli 3
+
+# liejot : slicing mēs neiegūstam kļūdas var rakstīt arī it kā muļķibas
+
+print(saraksts[-5000:9000]) # ja nav tik daudz elementu, vienkārši atgriež visu kas ir
+
+# kā būtu ar apgrieztu sarakstu? tam var izmanot negatīvu indeksu
+aggriezts = skaitli[::-1] # no sākuma līdz beigām ar soli -1
+print(aggriezts) # jauns saraksts kas sākas ar 19 un iet uz leju
