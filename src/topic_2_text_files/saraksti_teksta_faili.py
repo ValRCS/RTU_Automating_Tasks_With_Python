863be389402d51f7ccbeb6051e80f031095afe1c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-01-20 18:30:15 +0200

    Enhance saraksti_teksta_faili.py to include examples of variable usage and expand on the importance of lists in Python.

--- src/topic_2_text_files/saraksti_teksta_faili.py
+++ src/topic_2_text_files/saraksti_teksta_faili.py
@@ -5,3 +5,22 @@
 print("Plāns šodienai:")
 print("- Apskatīt saraksta datu struktūru Python valodā")
 print("- Apskatīt teksta failus un to apstrādi Python valodā")
+
+# Kāpēc vajdzīgi saraksti?
+
+# mēs tāču mākam izmantot mainīgos, vai ne?
+# piemēram:
+a1 = "Oskars"
+a2 = "Jānis"
+a3 = "Pēteris"
+a4 = "Anna"
+
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+
+# ko darīt ja mums jāglabā 1000 vārdi?
+# vai 10 000 vārdi?
+# vai pat 1 000 000 vārdi?
+# vai vēl vairāk?
